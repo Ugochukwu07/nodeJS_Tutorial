d71de970a9bf88f509f2fe909dd7f6c368a77ee4
Ekwueme Ugochukwu <ekwuemeugochukwu83@gmail.com>
2023-02-16 08:28:08 +0100

    new up;oafd

---
+++ app.js
@@ -0,0 +1,61 @@
+// const {readFileSync, writeFileSync} = require('fs');
+
+// const first = readFileSync('./content/first.txt', 'utf-8')
+// const secound = readFileSync('./content/secound.txt', 'utf-8')
+
+// console.log(first, secound)
+
+// writeFileSync(
+//     './content/third.txt', 
+//     `hello world ${first}, ${secound}`,
+//     {flag: 'a'}
+//     )
+
+// const path = require('path');
+
+// const file_path = path.join('/content', 'subFolder', 'text.txt')
+// // console.log(path.sep)
+// console.log(file_path)
+
+// const base = path.basename(file_path)
+// console.log(base)
+
+// const absolute = path.resolve(__dirname, 'content', 'subFolder', 'text.txt')
+// console.log(absolute)
+
+// const os = require('os')
+
+// console.log(os.userInfo().username)
+// console.log(`The system Uptime is ${os.uptime()} secounds`)
+
+// const currentOS = {
+//     name: os.type(),
+//     release: os.release(),
+//     totalMem: os.totalmem()/8/1024/1024,
+//     freeMem: os.freemem()
+// }
+
+// console.log(currentOS)
+// const {jonh, doe, sayHi} = require('./name')
+
+// sayHi('susan')
+// sayHi(jonh)
+// sayHi(doe)
+
+// const number = 1
+
+// if(number > 10){
+//     console.log('Number is smaller');
+// }else{
+//     console.log('Number is bigger')
+// }
+
+// console.log(__dirname)
+// let x = 0
+// setInterval(() => {
+//     if(x > 3){
+//         clearInterval(this)
+//     }
+//     console.log('hello world', x)
+//     x++
+// }, 1000)
---
+++ name.js
@@ -0,0 +1,9 @@
+
+const jonh = 'Jonh'
+const doe = 'Doe'
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+module.exports = {jonh, doe, sayHi}
